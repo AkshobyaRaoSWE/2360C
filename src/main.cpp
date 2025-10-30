20e40eb2898e26283ab075056af5324b2af0da95
Akshobya Rao <akshobyaraoglobal@gmail.com>
2025-10-30 15:45:38 -0500

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,101 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "lemlib/chassis/trackingWheel.hpp"
+#include "pros/motors.hpp"
 
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
+pros::Controller master(pros::E_CONTROLLER_MASTER);
+
+pros::MotorGroup left_motors({1, 2, 3}); 
+pros::MotorGroup right_motors({4, 5, 6}); 
+
+pros::Motor intake_motor_1(7);
+pros::Motor intake_motor_2(8);
+pros::Motor intake_motor_3(9);
+
+lemlib::Drivetrain drivetrain(&left_motors, 
+	&right_motors, 
+	10, // change
+	lemlib::Omniwheel::NEW_275, 
+	420,
+	2
+);
+
+pros::Imu imu(10);
+
+// odometry settings
+lemlib::OdomSensors sensors(nullptr, 
+	nullptr, 
+	nullptr, 
+	nullptr, 
+	&imu
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(
+	10, // proportional gain (kP)
+    0, // integral gain (kI)
+    3, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in inches
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in inches
+    500, // large error range timeout, in milliseconds
+    20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(
+	2, // proportional gain (kP)
+    0, // integral gain (kI)
+    10, // derivative gain (kD)
+    3, // anti windup
+    1, // small error range, in degrees
+    100, // small error range timeout, in milliseconds
+    3, // large error range, in degrees
+    500, // large error range timeout, in milliseconds
+    0 // maximum acceleration (slew)
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttle_curve(
+	3, 
+	10, 
+	1.019 
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steer_curve(
+	3, 
+	10, 
+	1.019 
+);
+
+// create the chassis
+lemlib::Chassis chassis(
+	drivetrain,
+	lateral_controller,
+	angular_controller,
+	sensors,
+	&throttle_curve, 
+	&steer_curve
+);	
+
+void on_center_button() {}
 
 
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+	pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+	pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
 }
 
 void disabled() {}
@@ -26,21 +105,14 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
-
 	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
 
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
+		int leftY = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+		int rightX = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+
+		left_motors.move(leftY);
+		right_motors.move(rightX);
+
+		pros::delay(20);
 	}
 }
